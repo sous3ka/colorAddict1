3f74876cad666c5eeeb8b65c8e48b1cd46964586
0xGali <150254949+0xGali@users.noreply.github.com>
2023-11-08 15:17:39 +0100

    Update Joker.java

--- Joker.java
+++ Joker.java
@@ -2,7 +2,6 @@ public class Joker extends Carte{
 
     public Joker()
     {
-        //couleur par défaut du joker (sans importance grace à l'utilisation du instanceof)
-        super(Couleur.ROUGE,Couleur.ROUGE);
+        super(null,null);
     }
 }
