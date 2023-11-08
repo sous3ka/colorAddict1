08e5616283e711fedbcc859499cf51c15a680b50
0xGali <150254949+0xGali@users.noreply.github.com>
2023-11-08 14:59:20 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -1,17 +1,40 @@
 import java.util.LinkedList;
 public class Partie {
-    public LinkedList<Carte> piocheJ;
-    public LinkedList<Carte> piocheO;
+    public LinkedList<Carte> pioche1;
+    public LinkedList<Carte> pioche2;
+    public LinkedList<Carte> pioche3;
+    public LinkedList<Carte> pioche4;
+    public LinkedList<Carte> pioche5;
+    public LinkedList<Carte> pioche6;
     public LinkedList<Carte> jeu;
     public Carte actuel;
+    public int tour;
+    public int nb_joueurs;
+
+    //fonction qui change la valeur du booleen tour
+    public void change()
+    {
+        if(tour == true)
+        {
+            tour = false;
+        }
+        else
+        {
+            tour = true;
+        }
+    }
 
     public void jouer(Carte c)
     {
         if(compare(actuel,c))
         {
             actuel = c;
+            change();
         }
     }
+
+
+
 
     //fonction pour vérifier si l'on peut jouer la carte c2 sur la carte c1
     //c1 ne peut donc pas être un joker car elle est déja sur la table
