9aedcee9e5df3ffbd8e724dbbdd691f1dada5bcf
0xGali <150254949+0xGali@users.noreply.github.com>
2023-11-15 17:08:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-    Carte c1 = new Carte(Couleur.ROUGE, Couleur.BLEU);
-    Carte c2 = new Carte(Couleur.BLEU, Couleur.ROUGE);
-    System.out.println(Partie.compare(c1,c2));
+        Partie p = new Partie(2);
+        for(int i = 0; i < p.jeu.size();i++)
+        {
+            System.out.println(p.jeu.get(i).toString());
+        }
     }
 }
