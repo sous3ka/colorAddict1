bf716ed9249822a9818e7a690088b2cdf22b7552
0xGali <150254949+0xGali@users.noreply.github.com>
2023-11-15 16:59:28 +0100

    Update Partie.java

--- Partie.java
+++ Partie.java
@@ -8,6 +8,14 @@ public class Partie {
     public int tour; //indice du joueur qui joue
     public int nb_joueurs;
 
+    //constructeur de la partie, reçoit en paramètre le nombre de joueurs de la partie
+    //prepare la partie (création + distribution des cartes + choix aléatoire du premier joueur)
+    public Partie(int nbj)
+    {
+        nb_joueurs = nbj;
+        creerDeck();
+        tour = (int)(Math.random() * nbj);
+    }
     //fonction qui change la valeur du tour
     public void change()
     {
@@ -18,15 +26,17 @@ public class Partie {
         }
     }
 
+    //permet au joueur actuel de jouer la carte c SEULEMENT SI IL LE PEUT
     public void jouer(Carte c)
     {
         if(compare(actuel,c))
         {
             actuel = c;
-            change();
+            piocher();
         }
     }
 
+    //permet au joueur actuel de piocher SEULEMENT SI IL LE PEUT
     public void piocher()
     {
         if(Decks.get(tour).get(0) != null)
@@ -34,6 +44,7 @@ public class Partie {
             Mains.get(tour).add(Decks.get(tour).get(0));
             Decks.get(tour).remove(0);
         }
+        change();
     }
 
     //fonction pour vérifier si l'on peut jouer la carte c2 sur la carte c1
@@ -46,8 +57,9 @@ public class Partie {
         ||c1.ecrit == c2.ecrit
         ||c2 instanceof Joker);
     }
-    
+
     public void creerDeck() {
+        jeu = new LinkedList<Carte>();
         for (Couleur c1 : Couleur.values()) {
             for (Couleur c2 : Couleur.values()) {
                 for (int exemplaire = 1; exemplaire <= 2; exemplaire++) {
@@ -58,7 +70,6 @@ public class Partie {
         }
         Carte carteJoker = new Joker();
         for (int i=0;i<12;i++){
-
             jeu.add(carteJoker);
         }
     }
