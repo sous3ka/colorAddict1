49c97fd54136e57b9392afaa868476d5949dad48
0xGali <150254949+0xGali@users.noreply.github.com>
2023-11-15 17:43:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         Partie p = new Partie(2);
-        for(int i = 0; i < p.jeu.size();i++)
+        for(int i = 0; i < p.Decks.get(0).size();i++)
         {
-            System.out.println(p.jeu.get(i).toString());
+            System.out.println(p.Decks.get(0).get(i).toString()+" "+i);
+        }
+
+        for(int i = 0; i < p.Decks.get(1).size();i++)
+        {
+            System.out.println(p.Decks.get(1).get(i).toString()+" "+i);
+        }
+
+        System.out.println(p.actuel.toString());
+
+        for(int i = 0; i < p.Mains.get(0).size();i++)
+        {
+            System.out.println(p.Mains.get(0).get(i).toString()+" "+i);
+        }
+
+        for(int i = 0; i < p.Mains.get(1).size();i++)
+        {
+            System.out.println(p.Mains.get(1).get(i).toString()+" "+i);
         }
     }
 }
