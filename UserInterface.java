fa80c51d8bb3cbc5aca3fdd527bab0b7ffbf7d87
sous3ka <150255012+sous3ka@users.noreply.github.com>
2023-12-16 16:52:35 +0100

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,28 +1,30 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class UserInterface {
+
 
     static void creerPage(JFrame page) {
         //JFrame pagePrincipale = new JFrame();//Créé la page
         page.setVisible(true);//Assure la visibilité de la page
         page.setLayout(null);//Permet de gérer manuellement tout les composants de la page
-        page.setSize(1000, 700);//Taille de la page
+        page.setSize(1000, 720);//Taille de la page
         page.setTitle("Le jeu des couleurs");//Titre de la page
-        //page.setResizable(false);
+        page.setResizable(false);
         page.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Assure que lorsqu'on clique sur la croix, la page se ferme et ne consomme plus de RAM
     }
 
     static void pageAccueil(JFrame page){
         JButton boutonJouer = new JButton("Jouer");
-        boutonJouer.setBounds(350,275,100,50);
+        boutonJouer.setBounds(454,310,120,80);
         JLabel textJouer = new JLabel("Lancer le jeu");
         JTextArea nomJoueur = new JTextArea("joueur");
 
-        nomJoueur.setBounds(350,100,100,30);
-        textJouer.setBounds(362,225,75,50);
+        nomJoueur.setBounds(450,150,100,30);
+        textJouer.setBounds(470,225,75,50);
 
         boutonJouer.addActionListener(new ActionListener() {
             @Override
@@ -41,53 +43,73 @@ public class UserInterface {
     }
 
     static void jeu1(JFrame page, String nomJoueurString, Partie enCours){
-        //faire les tours de jeu + les images cartes
-        //1ere idée : faire des zones pour les cartes +
-        // au dessus des boutons pour les jouer si la carte est compatible
 
-        //JPanel panelJoueur = new JPanel();
-        //JPanel panelAdversaire = new JPanel();
 
         JLabel nomAdversaire = new JLabel("Adversaire");
         nomAdversaire.setHorizontalAlignment(SwingConstants.CENTER);
-        nomAdversaire.setBounds(350,20,100,30);
+        nomAdversaire.setBounds(450,10,100,30);
 
         JLabel nomJoueur = new JLabel(nomJoueurString);
         nomJoueur.setHorizontalAlignment(SwingConstants.CENTER);
-        nomJoueur.setBounds(350,500,100,30);
+        nomJoueur.setBounds(450,650,100,30);
 
-        //panelJoueur.add(nomJoueur);
-        //panelAdversaire.add(nomAdversaire);
-        //panelAdversaire.setBounds(100,50,300,250);Mettre les éléments dans des JPanel afin de mieux les séparer et les distinguer mais je n'y arrive aps.
 
+
+        ImageIcon carteActuelleImage = ImageUtilitaire.createImageIcon(ImageUtilitaire.imageCartes(enCours.actuel),"carteActuelle");
         JLabel carteActuelle = new JLabel();
-        carteActuelle.setBounds(365,200,200,100);
-        carteActuelle.setOpaque(true);
-        carteActuelle.setBackground(Color.BLUE);
-        carteActuelle.setText(enCours.actuel.toString());
 
-        JLabel carteAdversaire = new JLabel();
-        carteAdversaire.setBounds(150,100,200,100);
-        carteAdversaire.setOpaque(true);
-        carteAdversaire.setBackground(Color.GREEN);
-        carteAdversaire.setText(enCours.Mains.get(enCours.tour).get(0).toString());
+        carteActuelle.setBounds(460,280,80,125);
+        carteActuelleImage.setImage(carteActuelleImage.getImage().getScaledInstance((carteActuelle.getWidth()),carteActuelle.getHeight(),Image.SCALE_SMOOTH));
+        carteActuelle.setIcon(carteActuelleImage);
 
-        JButton carteJoueur1 = new JButton("blablabla");
-        JButton carteJoueur2 = new JButton("blablabla");
-        JButton carteJoueur3 = new JButton("blablabla");
+        //carteActuelle.setText(enCours.actuel.toString());
 
-        carteJoueur1.setBounds(100,350,200,100);
-        carteJoueur2.setBounds(310,350,200,100);
-        carteJoueur3.setBounds(420,350,200,100);
+        JLabel carteAdversaire1 = new JLabel();
+        JLabel carteAdversaire2 = new JLabel();
+        JLabel carteAdversaire3 = new JLabel();
+        carteAdversaire1.setBounds(350,50,80,125);
+        carteAdversaire2.setBounds(450,50,80,125);
+        carteAdversaire3.setBounds(550,50,80,125);
+        carteAdversaire1.setOpaque(true);
+        carteAdversaire2.setOpaque(true);
+        carteAdversaire3.setOpaque(true);
+        carteAdversaire1.setBackground(Color.GREEN);
+        carteAdversaire2.setBackground(Color.BLUE);
+        carteAdversaire3.setBackground(Color.PINK);
+        carteAdversaire1.setText(enCours.Mains.get(1).get(0).toString());
+        carteAdversaire2.setText(enCours.Mains.get(1).get(1).toString());
+        carteAdversaire3.setText(enCours.Mains.get(1).get(2).toString());
 
-        carteJoueur1.setText(enCours.Mains.get(enCours.tour).get(0).toString());
-        carteJoueur1.setBackground(Color.ORANGE);
+        JButton carteJoueur1 = new JButton();
+        JButton carteJoueur2 = new JButton();
+        JButton carteJoueur3 = new JButton();
 
-        carteJoueur2.setText(enCours.Mains.get(enCours.tour).get(1).toString());
-        carteJoueur2.setBackground(Color.RED);
+        carteJoueur1.setBounds(350,520,80,125);
+        carteJoueur2.setBounds(450,520,80,125);
+        carteJoueur3.setBounds(550,520,80,125);
 
-        carteJoueur3.setText(enCours.Mains.get(enCours.tour).get(2).toString());
-        carteJoueur3.setBackground(Color.PINK);
+        ImageIcon carteJoueur1Image = ImageUtilitaire.createImageIcon(ImageUtilitaire.imageCartes(enCours.Mains.get(0).get(0)),"carte 1 joueur1");
+        carteJoueur1Image.setImage(carteJoueur1Image.getImage().getScaledInstance((carteJoueur1.getWidth()),carteJoueur1.getHeight(),Image.SCALE_SMOOTH));
+        ImageIcon carteJoueur2Image = ImageUtilitaire.createImageIcon(ImageUtilitaire.imageCartes(enCours.Mains.get(0).get(1)),"carte 2 joueur1");
+        carteJoueur2Image.setImage(carteJoueur2Image.getImage().getScaledInstance((carteJoueur2.getWidth()),carteJoueur2.getHeight(),Image.SCALE_SMOOTH));
+        ImageIcon carteJoueur3Image = ImageUtilitaire.createImageIcon(ImageUtilitaire.imageCartes(enCours.Mains.get(0).get(2)),"carte 3 joueur1");
+        carteJoueur3Image.setImage(carteJoueur3Image.getImage().getScaledInstance((carteJoueur3.getWidth()),carteJoueur3.getHeight(),Image.SCALE_SMOOTH));
+
+        carteJoueur1.setIcon(carteJoueur1Image);
+        carteJoueur2.setIcon(carteJoueur2Image);
+        carteJoueur3.setIcon(carteJoueur3Image);
+        //carteJoueur1.setText(enCours.Mains.get(0).get(0).toString());
+        //carteJoueur1.setBackground(Color.ORANGE);
+
+        //carteJoueur2.setText(enCours.Mains.get(0).get(1).toString());
+        //carteJoueur2.setBackground(Color.RED);
+
+        //carteJoueur3.setText(enCours.Mains.get(0).get(2).toString());
+        //carteJoueur3.setBackground(Color.PINK);
+
+        JButton piocheJoueur = new JButton();
+        piocheJoueur.setBounds(650,520,80,60);
+        piocheJoueur.setText(enCours.Decks.get(0).get(1).toString());
 
         carteJoueur1.addActionListener(new ActionListener() {
             @Override
@@ -119,7 +141,30 @@ public class UserInterface {
                 jeu1(page, nomJoueurString,enCours);
             }
         });
+
+        piocheJoueur.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                enCours.piocher();
+                page.getContentPane().removeAll();
+                page.revalidate();
+                page.repaint();
+                jeu1(page, nomJoueurString,enCours);
+            }
+        });
+
         System.out.println("tour du joueur: "+enCours.tour);
+
+        if (enCours.tour == 1){
+            System.out.println("Tour ordinateur");
+            enCours.tour_ordinateur();
+            page.getContentPane().removeAll();
+            page.revalidate();
+            page.repaint();
+            jeu1(page,nomJoueurString,enCours);
+        }
+
+
 
 /*        if (enCours.compare(enCours.actuel,enCours.Mains.get(enCours.tour).get(0))){
             carteJoueur1.addActionListener(new ActionListener() {
@@ -136,10 +181,20 @@ public class UserInterface {
 
         page.add(carteActuelle);
 
-        page.add(carteAdversaire);
+        page.add(carteAdversaire1);
+        page.add(carteAdversaire2);
+        page.add(carteAdversaire3);
 
         page.add(carteJoueur1);
         page.add(carteJoueur2);
         page.add(carteJoueur3);
+
+        page.add(piocheJoueur);
     }
+
+
+
+
+
+
 }
