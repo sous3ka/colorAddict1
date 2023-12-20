95185d9c32ae7d6bf97b20dbc10a2e4f431cc609
sous3ka <150255012+sous3ka@users.noreply.github.com>
2023-12-20 07:48:30 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        //Partie p = new Partie(3);
         JFrame pagePrincipale = new JFrame();
-        //UserInterface.imageCartes();
-        UserInterface2.creerPage(pagePrincipale);
-        UserInterface2.pageAccueil(pagePrincipale);
-
-/*        for(int i = 0; i < p.jeu.size();i++)
-        {
-            System.out.println(p.jeu.get(i).toString() + i);
-        }*/
+        UserInterface.creerPage(pagePrincipale);
+        UserInterface.pageAccueil(pagePrincipale);
     }
 }
